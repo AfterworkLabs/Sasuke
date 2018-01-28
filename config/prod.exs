b5fb3d016f2559e535be394306c76f24ce23824f
Shawn Feng <shawnsfeng@gmail.com>
2018-01-28 07:48:42 -0800

    fix config file typo

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :sasuke, SasukeWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: false
 
 # Do not print debug messages in production
@@ -29,7 +29,7 @@ username: "${DB_USER}",
 password: "${DB_PASSWORD}",
 database: "${DB_NAME}",
 hostname: "${DB_HOST}",
-pool_size: 50
+pool_size: 18
 
 # ## SSL Support
 #
